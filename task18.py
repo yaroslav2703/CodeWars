d4d4a0dfdc0b602ab35c44d530c1bf5f3a4c0f6c
yarvald <yaroslav.pitsukha@gmail.com>
2020-03-17 13:22:50 +0300

    new tasks

--- task18.py
+++ task18.py
@@ -5,6 +5,7 @@ def sum_of_intervals(intervals):
     a = sorted(list(set(a)))
     return len(a)
 
+
 print(sum_of_intervals([(1, 4), (7, 10), (3, 5)]))
 print(sum_of_intervals([(1, 5), (6, 10)]))
 print(sum_of_intervals([(1, 5), (1, 5)]))
